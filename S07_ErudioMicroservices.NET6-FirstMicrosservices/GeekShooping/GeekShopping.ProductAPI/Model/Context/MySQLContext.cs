01eb191c87eba35168aa50619b152a4a78f3544c
leandrocgsi <leandrocgsi@gmail.com>
2021-08-06 23:31:57 -0300

    Creating our First Microservice

---
+++ S07_ErudioMicroservices.NET6-FirstMicrosservices/GeekShooping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext() {}
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
