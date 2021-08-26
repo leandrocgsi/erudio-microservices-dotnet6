0a0e198dad235f0d9f07ff5c950e86b1cf363ec9
leandrocgsi <leandrocgsi@gmail.com>
2021-08-26 17:45:03 -0300

    Authentication with Oauth2, OpenID, Duende and Identity Server

--- S11_ErudioMicroservices.NET6-AuthenticationWithOauth2OpenIDAndIdentityServer/GeekShooping/GeekShopping.IdentityServer/Startup.cs
+++ S11_ErudioMicroservices.NET6-AuthenticationWithOauth2OpenIDAndIdentityServer/GeekShooping/GeekShopping.IdentityServer/Startup.cs
@@ -1,4 +1,5 @@
 using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Initializer;
 using GeekShopping.IdentityServer.Model;
 using GeekShopping.IdentityServer.Model.Context;
 using Microsoft.AspNetCore.Builder;
@@ -48,6 +49,8 @@ namespace GeekShopping.IdentityServer
                     .AddInMemoryClients(IdentityConfiguration.Clients)
                     .AddAspNetIdentity<ApplicationUser>();
 
+            services.AddScoped<IDbInitializer, DbInitializer>();
+
             builder.AddDeveloperSigningCredential();
 
             services.AddControllersWithViews();
@@ -55,7 +58,8 @@ namespace GeekShopping.IdentityServer
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, 
-            IWebHostEnvironment env
+            IWebHostEnvironment env,
+            IDbInitializer initializer
         )
         {
             if (env.IsDevelopment())
@@ -72,6 +76,8 @@ namespace GeekShopping.IdentityServer
             app.UseRouting();
             app.UseIdentityServer();
             app.UseAuthorization();
+
+            initializer.Initialize();
 
             app.UseEndpoints(endpoints =>
             {
