3eb3a010e98858970c63abb5c250f56acf225d6c
leandrocgsi <leandrocgsi@gmail.com>
2021-10-27 14:06:31 -0300

    Implementing Calculator

--- S05_ErudioMicroservices.NET6-REST_Recap/02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ S05_ErudioMicroservices.NET6-REST_Recap/02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RestWithASPNETUdemy.Controllers
 {
