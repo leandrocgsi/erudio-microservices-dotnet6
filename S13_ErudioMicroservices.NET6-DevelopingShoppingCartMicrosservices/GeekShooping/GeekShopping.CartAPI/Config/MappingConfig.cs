e84f1db2903ba7bd27b931228d036d19315701d7
leandrocgsi <leandrocgsi@gmail.com>
2021-10-28 15:44:24 -0300

    Developing GeekShopping.ShoppingCart Microservice

---
+++ S13_ErudioMicroservices.NET6-DevelopingShoppingCartMicrosservices/GeekShooping/GeekShopping.CartAPI/Config/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GeekShopping.CartAPI.Model;
+
+namespace GeekShopping.CartAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                //config.CreateMap<ProductVO, Product>();
+                //config.CreateMap<Product, ProductVO>();
+            });
+            return mappingConfig;
+        }
+    }
+}
