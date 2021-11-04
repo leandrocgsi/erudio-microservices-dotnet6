fd17d8505a58ba250f958ac8d790a1ffb802d67c
leandrocgsi <leandrocgsi@gmail.com>
2021-11-04 11:01:40 -0300

    Consuming GeekShopping.CartAPI Microservice

---
+++ S14_ErudioMicroservices.NET6-ConsumingShoppingCartMicrosservice/GeekShooping/GeekShopping.CartAPI/Model/CartDetail.cs
@@ -0,0 +1,20 @@
+﻿using GeekShopping.CartAPI.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.CartAPI.Model
+{
+    [Table("cart_detail")]
+    public class CartDetail : BaseEntity
+    {
+        public long CartHeaderId { get; set; }
+
+        [ForeignKey("CartHeaderId")]
+        public virtual CartHeader CartHeader { get; set; }
+        public long ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+
+        [Column("count")]
+        public int Count { get; set; }
+    }
+}
---
+++ S14_ErudioMicroservices.NET6-ConsumingShoppingCartMicrosservice/GeekShooping/GeekShopping.CartAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartDetail> CartDetails { get; set; }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+    }
+}
