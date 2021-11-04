09b5ab5ab2998fecc4121ff319419b3650d21e72
leandrocgsi <leandrocgsi@gmail.com>
2021-11-04 10:59:53 -0300

    Developing GeekShopping.CartAPI Microservice

---
+++ S13_ErudioMicroservices.NET6-DevelopingShoppingCartMicrosservice/GeekShooping/GeekShopping.CartAPI/Controllers/CartController.cs
@@ -0,0 +1,57 @@
+﻿using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekShopping.CartAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CartController : ControllerBase
+    {
+        private ICartRepository _repository;
+
+        public CartController(ICartRepository repository)
+        {
+            _repository = repository ?? throw new
+                ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("find-cart/{id}")]
+        public async Task<ActionResult<CartVO>> FindById(string userId)
+        {
+            var cart = await _repository.FindCartByUserId(userId);
+            if (cart == null) return NotFound();
+            return Ok(cart);
+        }
+
+        [HttpPost("add-cart/{id}")]
+        public async Task<ActionResult<CartVO>> AddCart(CartVO vo)
+        {
+            var cart = await _repository.SaveOrUpdateCart(vo);
+            if (cart == null) return NotFound();
+            return Ok(cart);
+        }
+
+        [HttpPut("update-cart/{id}")]
+        public async Task<ActionResult<CartVO>> UpdateCart(CartVO vo)
+        {
+            var cart = await _repository.SaveOrUpdateCart(vo);
+            if (cart == null) return NotFound();
+            return Ok(cart);
+        }
+
+        [HttpDelete("remove-cart/{id}")]
+        public async Task<ActionResult<CartVO>> RemoveCart(int id)
+        {
+            var status = await _repository.RemoveFromCart(id);
+            if (!status) return BadRequest();
+            return Ok(status);
+        }
+    }
+}
---
+++ S13_ErudioMicroservices.NET6-DevelopingShoppingCartMicrosservice/GeekShooping/GeekShopping.IdentityServer/Initializer/IDbInitializer.cs
@@ -0,0 +1,7 @@
+﻿namespace GeekShopping.IdentityServer.Initializer
+{
+    public interface IDbInitializer
+    {
+        public void Initialize();
+    }
+}
