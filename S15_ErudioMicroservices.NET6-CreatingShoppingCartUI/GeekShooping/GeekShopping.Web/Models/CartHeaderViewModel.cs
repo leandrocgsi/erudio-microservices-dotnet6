5beea21d520236ec444afce580dcb77a52865558
leandrocgsi <leandrocgsi@gmail.com>
2021-11-05 23:27:39 -0300

    Creating Shopping Cart UI

--- S15_ErudioMicroservices.NET6-CreatingShoppingCartUI/GeekShooping/GeekShopping.Web/Models/CartHeaderViewModel.cs
+++ S15_ErudioMicroservices.NET6-CreatingShoppingCartUI/GeekShooping/GeekShopping.Web/Models/CartHeaderViewModel.cs
@@ -5,6 +5,6 @@
         public long Id { get; set; }
         public string UserId { get; set; }
         public string CouponCode { get; set; }
-        public double PurchaseAmount { get; set; }
+        public decimal PurchaseAmount { get; set; }
     }
 }
