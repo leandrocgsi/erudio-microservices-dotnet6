e9c9c9416565d8c2a82598c964661ffc6c0922d4
leandrocgsi <leandrocgsi@gmail.com>
2021-11-07 11:57:05 -0300

    Consuming GeekShopping.CouponAPI Microservice

---
+++ S17_ErudioMicroservices.NET6-ConsumingGeekShopping.CouponAPIMicroservice/GeekShooping/GeekShopping.CartAPI/Model/CartHeader.cs
@@ -0,0 +1,15 @@
+﻿using GeekShopping.CartAPI.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.CartAPI.Model
+{
+    [Table("cart_header")]
+    public class CartHeader : BaseEntity
+    {
+        [Column("user_id")]
+        public string UserId { get; set; }
+
+        [Column("coupon_code")]
+        public string CouponCode { get; set; }
+    }
+}
---
+++ S17_ErudioMicroservices.NET6-ConsumingGeekShopping.CouponAPIMicroservice/GeekShooping/GeekShopping.CartAPI/Model/Product.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.CartAPI.Model
+{
+    [Table("product")]
+    public class Product
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("name")]
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Column("price")]
+        [Required]
+        [Range(1,10000)]
+        public decimal Price { get; set; }
+
+        [Column("description")]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Column("category_name")]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [Column("image_url")]
+        [StringLength(300)]
+        public string ImageURL { get; set; }
+    }
+}
