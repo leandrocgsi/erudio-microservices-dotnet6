28894fc4feda9282c4ea7cf6e8d924641f3e231c
leandrocgsi <leandrocgsi@gmail.com>
2021-11-26 09:53:57 -0300

    Integrating Microservices to RabbitMQ

---
+++ S20_ErudioMicroservices.NET6-IntegratingMicroservicesToRabbitMQ/GeekShooping/GeekShopping.CouponAPI/Model/Base/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.CouponAPI.Model.Base
+{
+    public class BaseEntity
+    {
+
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+    }
+}
---
+++ S20_ErudioMicroservices.NET6-IntegratingMicroservicesToRabbitMQ/GeekShooping/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using System.Collections.Generic;
+
+namespace GeekShopping.IdentityServer.Configuration
+{
+    public static class IdentityConfiguration
+    {
+        public const string Admin = "Admin";
+        public const string Client = "Client";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            { 
+                new ApiScope("geek_shopping", "GeekShopping Server"),
+                new ApiScope(name: "read", "Read data."),
+                new ApiScope(name: "write", "Write data."),
+                new ApiScope(name: "delete", "Delete data."),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("my_super_secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"read", "write", "profile" }
+                },
+                new Client
+                {
+                    ClientId = "geek_shopping",
+                    ClientSecrets = { new Secret("my_super_secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = {"https://localhost:4430/signin-oidc"},
+                    PostLogoutRedirectUris = {"https://localhost:4430/signout-callback-oidc"},
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "geek_shopping"
+                    }
+                }
+            };
+    }
+}
