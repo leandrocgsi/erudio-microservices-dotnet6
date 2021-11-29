e0cf7ca522f6c5553aa21a904e287a53ae8a5b1b
leandrocgsi <leandrocgsi@gmail.com>
2021-11-29 16:14:13 -0300

    Creating Microservice GeekShopping.PaymentAPI

--- S23_ErudioMicroservices.NET6-CreatingPaymentAPIMicroservice/GeekShooping/GeekShopping.CartAPI/Repository/CartRepository.cs
+++ S23_ErudioMicroservices.NET6-CreatingPaymentAPIMicroservice/GeekShooping/GeekShopping.CartAPI/Repository/CartRepository.cs
@@ -69,7 +69,7 @@ namespace GeekShopping.CartAPI.Repository
             Cart cart = new()
             {
                 CartHeader = await _context.CartHeaders
-                    .FirstOrDefaultAsync(c => c.UserId == userId),
+                    .FirstOrDefaultAsync(c => c.UserId == userId) ?? new CartHeader(),
             };
             cart.CartDetails = _context.CartDetails
                 .Where(c => c.CartHeaderId == cart.CartHeader.Id)
