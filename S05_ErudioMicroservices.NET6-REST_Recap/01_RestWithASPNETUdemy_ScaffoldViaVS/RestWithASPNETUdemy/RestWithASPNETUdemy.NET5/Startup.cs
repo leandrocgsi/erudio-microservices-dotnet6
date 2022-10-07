9496be5f53f234ecac6a011a91019030825890fa
leandrocgsi <leandrocgsi@gmail.com>
2022-10-07 17:53:28 -0300

    Scaffolding a WebApi Project via Visual Studio - .NET 5

--- S05_ErudioMicroservices.NET6-REST_Recap/01_RestWithASPNETUdemy_ScaffoldViaVS/RestWithASPNETUdemy/RestWithASPNETUdemy.NET5/Startup.cs
+++ S05_ErudioMicroservices.NET6-REST_Recap/01_RestWithASPNETUdemy_ScaffoldViaVS/RestWithASPNETUdemy/RestWithASPNETUdemy.NET5/Startup.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +26,12 @@ namespace RestWithASPNETUdemy.NET5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestWithASPNETUdemy.NET5", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,16 +40,11 @@ namespace RestWithASPNETUdemy.NET5
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestWithASPNETUdemy.NET5 v1"));
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -49,7 +52,7 @@ namespace RestWithASPNETUdemy.NET5
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
