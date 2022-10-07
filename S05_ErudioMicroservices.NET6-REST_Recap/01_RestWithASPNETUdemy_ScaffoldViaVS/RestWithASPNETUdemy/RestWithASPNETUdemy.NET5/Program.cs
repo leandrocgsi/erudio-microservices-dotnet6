208d5cd5627f0c033c706e59140dd767ff99b419
leandrocgsi <leandrocgsi@gmail.com>
2022-10-07 10:43:27 -0300

    Scaffolding a WebApi Project via Visual Studio - .NET 5

--- S05_ErudioMicroservices.NET6-REST_Recap/01_RestWithASPNETUdemy_ScaffoldViaVS/RestWithASPNETUdemy/RestWithASPNETUdemy.NET5/Program.cs
+++ S05_ErudioMicroservices.NET6-REST_Recap/01_RestWithASPNETUdemy_ScaffoldViaVS/RestWithASPNETUdemy/RestWithASPNETUdemy.NET5/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RestWithASPNETUdemy
+namespace RestWithASPNETUdemy.NET5
 {
     public class Program
     {
--- S05_ErudioMicroservices.NET6-REST_Recap/01_RestWithASPNETUdemy_ScaffoldViaVS/RestWithASPNETUdemy/RestWithASPNETUdemy.NET5/Startup.cs
+++ S05_ErudioMicroservices.NET6-REST_Recap/01_RestWithASPNETUdemy_ScaffoldViaVS/RestWithASPNETUdemy/RestWithASPNETUdemy.NET5/Startup.cs
@@ -1,17 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RestWithASPNETUdemy
+namespace RestWithASPNETUdemy.NET5
 {
     public class Startup
     {
@@ -25,8 +23,7 @@ namespace RestWithASPNETUdemy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,8 +33,15 @@ namespace RestWithASPNETUdemy
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -45,7 +49,7 @@ namespace RestWithASPNETUdemy
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapRazorPages();
             });
         }
     }
