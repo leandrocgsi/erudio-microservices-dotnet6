1a6080d524430ba37de7db10248a363fc2084954
leandrocgsi <leandrocgsi@gmail.com>
2022-10-10 22:20:25 -0300

    Organizing The First Microsservice Architecture

--- S08_ErudioMicroservices.NET6-OrganizingTheFirstMicrosservicesArchitecture/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ S08_ErudioMicroservices.NET6-OrganizingTheFirstMicrosservicesArchitecture/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -3,6 +3,7 @@ using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
 using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,7 +26,10 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
+});
 
 var app = builder.Build();
 
