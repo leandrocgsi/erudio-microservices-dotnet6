0b378c0bac1625a6df161f4ad27187dada4fe3c6
leandrocgsi <leandrocgsi@gmail.com>
2022-10-14 17:17:33 -0300

    Consuming Product Microservice on Web Microservice

--- S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceonWebMicroservice/GeekShopping/GeekShopping.Web/Models/ErrorViewModel.cs
+++ S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceonWebMicroservice/GeekShopping/GeekShopping.Web/Models/ErrorViewModel.cs
@@ -1,10 +1,8 @@
-using System;
-
 namespace GeekShopping.Web.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
--- S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceonWebMicroservice/GeekShopping/GeekShopping.Web/Program.cs
+++ S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceonWebMicroservice/GeekShopping/GeekShopping.Web/Program.cs
@@ -1,21 +1,14 @@
-using GeekShopping.Web.Services.IServices;
-using GeekShopping.Web.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-builder.Services.AddHttpClient<IProductService, ProductService>(
-    c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])
-);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
 
@@ -23,6 +16,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
