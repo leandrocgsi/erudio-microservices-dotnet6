9eb9f6098d9c107644357bcf351edd7389ab1188
leandrocgsi <leandrocgsi@gmail.com>
2023-01-23 16:19:36 -0300

    Creating Microservice GeekShopping.PaymentAPI

--- S23_ErudioMicroservices.NET6-CreatingPaymentAPIMicroservice/GeekShooping/GeekShopping.Web/Program.cs
+++ S23_ErudioMicroservices.NET6-CreatingPaymentAPIMicroservice/GeekShooping/GeekShopping.Web/Program.cs
@@ -1,26 +1,62 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 
-namespace GeekShopping.Web
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient<IProductService, ProductService>(
+    c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])
+);
+builder.Services.AddHttpClient<ICartService, CartService>(c =>
+        c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CartAPI"])
+    );
+builder.Services.AddHttpClient<ICouponService, CouponService>(c =>
+        c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"])
+    );
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(options => {
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+})
+    .AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+    .AddOpenIdConnect("oidc", options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
+        options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.ClientId = "geek_shopping";
+        options.ClientSecret = "my_super_secret";
+        options.ResponseType = "code";
+        options.ClaimActions.MapJsonKey("role", "role", "role");
+        options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+        options.TokenValidationParameters.NameClaimType = "name";
+        options.TokenValidationParameters.RoleClaimType = "role";
+        options.Scope.Add("geek_shopping");
+        options.SaveTokens = true;
+                });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
         {
-            CreateHostBuilder(args).Build().Run();
+    app.UseExceptionHandler("/Home/Error");
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
