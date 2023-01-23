6dc4385a7fb0a2f991ab720c617a4faa50debdfe
leandrocgsi <leandrocgsi@gmail.com>
2023-01-22 21:47:52 -0300

    Consuming GeekShopping.CouponAPI Microservice,

--- S17_ErudioMicroservices.NET6-ConsumingGeekShopping.CouponAPIMicroservice/GeekShooping/GeekShopping.Web/Program.cs
+++ S17_ErudioMicroservices.NET6-ConsumingGeekShopping.CouponAPIMicroservice/GeekShooping/GeekShopping.Web/Program.cs
@@ -10,6 +10,9 @@ builder.Services.AddHttpClient<IProductService, ProductService>(
 builder.Services.AddHttpClient<ICartService, CartService>(c =>
         c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CartAPI"])
     );
+builder.Services.AddHttpClient<ICouponService, CouponService>(c =>
+        c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"])
+    );
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
